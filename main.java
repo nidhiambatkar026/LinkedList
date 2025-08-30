0028e54f078855fa1ad1727e5637aad600e833e6
nidhiambatkar026 <nidhiambatkar@gmail.com>
2025-08-30 17:30:16 +0530

    singly Linked List code added

---
+++ main.java
@@ -0,0 +1,11 @@
+public class main {
+    public static void main(String args[]){
+    LL List = new LL();
+    List.insertFirst(3);
+    List.insertFirst(2);
+    List.insertFirst(8);
+    List.insertFirst(17);
+
+    List.display();
+}
+}
