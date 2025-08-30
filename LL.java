ead8d1e97ed8607964572d08d780adf6c8abc868
nidhiambatkar026 <nidhiambatkar@gmail.com>
2025-08-30 17:23:55 +0530

    insertFirst and display function added

--- LL.java
+++ LL.java
@@ -6,6 +6,27 @@ public class LL{
     public LL(){
         this.Size = 0;
     }
+
+    public void insertFirst(int val){
+        Node node = new Node(val);
+        node.next = head;
+        head = node;
+
+        if(tail == null){
+            tail = head;
+        }
+        Size++;
+    }
+
+    public void display(){
+        Node temp = head;
+        while(temp!= null){
+            System.out.println(temp.value + "->");
+            temp = temp.next;
+        }
+        System.out.println("END");
+    }
+
     private class Node {
         private int value;
         private Node next;
